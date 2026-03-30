0bde9fc08614f225567ea89cfc4dd26e9b52ca2c
cuetazo <cuetosalazarsebas@gmail.com>
2026-03-30 18:10:45 -0500

    i have no mouth but need to scream - finish setup google oauth csmre

--- tracker-pay/src/app/(auth)/login.tsx
+++ tracker-pay/src/app/(auth)/login.tsx
@@ -1,10 +1,55 @@
-import LoginCard from "@/components/loginCard";
 import Entypo from "@expo/vector-icons/Entypo";
-import { useEffect } from "react";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  isErrorWithCode,
+  isSuccessResponse,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+
+GoogleSignin.configure({
+  webClientId:
+    "184071206390-jaqlpi38k4kko7qhjd4eorfos8mirf7a.apps.googleusercontent.com",
+  offlineAccess: false,
+  profileImageSize: 120,
+});
 
 export default function LoginScreen() {
   useEffect(() => {}, []);
+  const [state, setState] = useState<{ userInfo: any | null }>({
+    userInfo: null,
+  });
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      console.log("response", response);
+      if (isSuccessResponse(response)) {
+        setState({ userInfo: response.data });
+      } else {
+        // sign in was cancelled by user
+      }
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // Android only, play services not available or outdated
+            break;
+          default:
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View>
@@ -20,12 +65,19 @@ export default function LoginScreen() {
 
       <View>
         <Text>Sign in into your account</Text>
-        <View>
-          <LoginCard
-            content="Sign in with Google"
-            icon={require("@/assets/images/google-icon.png")}
-          />
-        </View>
+        <GoogleSigninButton
+          style={{ width: 192, height: 48 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={signIn}
+        />
+        {state.userInfo && (
+          <>
+            <Text>{state.userInfo.user?.name}</Text>
+            <Text>{state.userInfo.user?.email}</Text>
+            <Text>{state.userInfo.user?.id}</Text>
+          </>
+        )}
       </View>
     </View>
   );
