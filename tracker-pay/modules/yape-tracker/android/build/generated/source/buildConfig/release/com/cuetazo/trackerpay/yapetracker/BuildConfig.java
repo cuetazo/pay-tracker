7e71a1a43522ffe56b1210c157827a0b0fce8360
cuetazo <cuetosalazarsebas@gmail.com>
2026-07-10 23:59:23 -0500

    changes

---
+++ tracker-pay/modules/yape-tracker/android/build/generated/source/buildConfig/release/com/cuetazo/trackerpay/yapetracker/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cuetazo.trackerpay.yapetracker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.cuetazo.trackerpay.yapetracker";
+  public static final String BUILD_TYPE = "release";
+}
